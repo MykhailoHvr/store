c2326fee44c203e738f963dd7512f581c5c8483f
mykhailo <michaelh12r2@gmail.com>
2021-07-05 16:53:20 +0300

    Add an implementation of the bookstore in memory

---
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Store.Memory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[]
+        {
+            new Book(1,"Art of Programing"),
+            new Book(2,"Pro Git"),
+            new Book(3,"Pro ASP.NET Core MVC 2"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
