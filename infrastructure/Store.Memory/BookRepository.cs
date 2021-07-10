0213718652d4673851b4f2ac8c37edc2acaea651
mykhailo <michaelh12r2@gmail.com>
2021-07-10 15:59:02 +0300

    Show cart

--- infrastructure/Store.Memory/BookRepository.cs
+++ infrastructure/Store.Memory/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace Store.Memory
 {
@@ -13,6 +14,15 @@ namespace Store.Memory
             new Book(3,"ISBN1231231232", "A. Freeman" ,"Pro ASP.NET Core MVC 2","Now in its 7th edition, the best selling book on MVC is updated for ASP.NET Core MVC 2. It contains detailed explanations of the Core MVC functionality which enables developers to produce leaner, cloud optimized and mobile-ready applications for the .NET platform. This book puts ASP.NET Core MVC into context and dives deep into the tools and techniques required to build modern, cloud optimized extensible web applications. All the new MVC features are described in detail and the author explains how best to apply them to both new and existing projects.The ASP.NET Core MVC Framework is the latest evolution of Microsoft’s ASP.NET web platform, built on a completely new foundation. It represents a fundamental change to how Microsoft constructs and deploys web frameworks and is free of the legacy of earlier technologies such as Web Forms. ASP.NET Core MVC provides a 'host agnostic' framework and a high-productivity programming model that promotes cleaner code architecture, test-driven development, and powerful extensibility.",
                         52.35m),
         };
+
+        public Book[] GetAllByIds(IEnumerable<int> bookIds)
+        {
+            var foundBooks = from book in books
+                            join bookId in bookIds on book.Id equals bookId
+                            select book;
+
+            return foundBooks.ToArray();
+        }
 
         public Book[] GetAllByIsbn(string isbn)
         {
