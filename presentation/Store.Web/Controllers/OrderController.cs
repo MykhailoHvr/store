233e40db0c8b7664bffd1b6b73979ed0c6c3ee32
mykhailo <michaelh12r2@gmail.com>
2021-07-13 17:11:34 +0300

    Add methods for removing books from cart

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -75,6 +75,55 @@ namespace Store.Web.Controllers
             cart.TotalPrice = order.TotalPrice;
             HttpContext.Session.Set(cart);
 
+
+            return RedirectToAction("Index", "Book", new { id });
+        }
+
+        public IActionResult RemoveItem(int id)
+        {
+            Order order;
+            Cart cart;
+            if (HttpContext.Session.TryGetCart(out cart))
+            {
+                order = orderRepository.GetById(cart.OrderId);
+            }
+            else
+            {
+                order = orderRepository.Create();
+                cart = new Cart(order.Id);
+            }
+
+            var book = bookRepository.GetById(id);
+            order.RemoveItem(book, 1);
+            orderRepository.Update(order);
+
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+            HttpContext.Session.Set(cart);
+            return RedirectToAction("Index", "Book", new { id });
+        }
+
+        public IActionResult RemoveItems(int id)
+        {
+            Order order;
+            Cart cart;
+            if (HttpContext.Session.TryGetCart(out cart))
+            {
+                order = orderRepository.GetById(cart.OrderId);
+            }
+            else
+            {
+                order = orderRepository.Create();
+                cart = new Cart(order.Id);
+            }
+
+            var book = bookRepository.GetById(id);
+            order.RemoveItems(book);
+            orderRepository.Update(order);
+
+            cart.TotalCount = order.TotalCount;
+            cart.TotalPrice = order.TotalPrice;
+            HttpContext.Session.Set(cart);
             return RedirectToAction("Index", "Book", new { id });
         }
     }
