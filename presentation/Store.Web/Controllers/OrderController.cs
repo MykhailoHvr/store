3ee13a1a2f96a2d9b253f63ec81d340de22ca941
mykhailo <michaelh12r2@gmail.com>
2021-07-16 16:34:45 +0300

    Confirm phone number

--- presentation/Store.Web/Controllers/OrderController.cs
+++ presentation/Store.Web/Controllers/OrderController.cs
@@ -1,6 +1,10 @@
 ﻿using System.Linq;
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
 using Store.Web.Models;
+using Store.Messages;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
 
 namespace Store.Web.Controllers
 {
@@ -8,14 +12,18 @@ namespace Store.Web.Controllers
     {
         private readonly IBookRepository bookRepository;
         private readonly IOrderRepository orderRepository;
+        private readonly INotificationService notificationService;
 
         public OrderController(IBookRepository bookRepository,
-                              IOrderRepository orderRepository)
+                              IOrderRepository orderRepository,
+                               INotificationService notificationService)
         {
             this.bookRepository = bookRepository;
             this.orderRepository = orderRepository;
+            this.notificationService = notificationService;
         }
 
+        [HttpGet]
         public IActionResult Index()
         {
             if (HttpContext.Session.TryGetCart(out Cart cart))
@@ -53,6 +61,7 @@ namespace Store.Web.Controllers
             };
         }
 
+        [HttpPost]
         public IActionResult AddItem(int bookId, int count = 1)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
@@ -92,6 +101,7 @@ namespace Store.Web.Controllers
             HttpContext.Session.Set(cart);
         }
 
+        [HttpPost]
         public IActionResult RemoveItem(int bookId)
         {
             (Order order, Cart cart) = GetOrCreateOrderAndCart();
@@ -101,6 +111,77 @@ namespace Store.Web.Controllers
             SaveOrderAndCart(order, cart);
 
             return RedirectToAction("Index", "Order");
+        }
+
+
+        [HttpPost]
+        public IActionResult SendConfirmationCode(int id, string cellPhone)
+        {
+            var order = orderRepository.GetById(id);
+            var model = Map(order);
+
+            if (!IsValidCellPhone(cellPhone))
+            {
+                model.Errors["cellPhone"] = "The phone number does not match the format +01234567890";
+                return View("Index", model);
+            }
+
+            int code = 1111; // random.Next(1000, 10000)
+            HttpContext.Session.SetInt32(cellPhone, code);
+            notificationService.SendConfirmationCode(cellPhone, code);
+
+            return View("Confirmation",
+                        new ConfirmationModel
+                        {
+                            OrderId = id,
+                            CellPhone = cellPhone
+                        });
+        }
+
+        private bool IsValidCellPhone(string cellPhone)
+        {
+            if (cellPhone == null)
+                return false;
+
+            cellPhone = cellPhone.Replace(" ", "")
+                                 .Replace("-", "");
+
+            return Regex.IsMatch(cellPhone, @"^\+?\d{11}$");
+        }
+
+        [HttpPost]
+        public IActionResult StartDelivery(int id, string cellPhone, int code)
+        {
+            int? storedCode = HttpContext.Session.GetInt32(cellPhone);
+            if (storedCode == null)
+            {
+                return View("Confirmation",
+                            new ConfirmationModel
+                            {
+                                OrderId = id,
+                                CellPhone = cellPhone,
+                                Errors = new Dictionary<string, string>
+                                {
+                                    { "code", "Empty code, resend" }
+                                },
+                            }); ;
+            }
+
+            if (storedCode != code)
+            {
+                return View("Confirmation",
+                            new ConfirmationModel
+                            {
+                                OrderId = id,
+                                CellPhone = cellPhone,
+                                Errors = new Dictionary<string, string>
+                                {
+                                    { "code", "Different from the one you sent" }
+                                },
+                            }); ;
+            }
+
+            return View();
         }
     }
 }
